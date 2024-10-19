349f9cebc5ca975d5946e2543ef96c61a583a3a9
CristhianCanar <cristhian.canar.m@gmail.com>
2024-10-19 16:40:54 -0500

    Listado de productos, implementacion de bootstrap

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { TodoApp } from './TodoApp'
 import { ListaProductos } from './ListaProductos'
 import 'bootstrap/dist/css/bootstrap.min.css';
